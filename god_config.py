63dc8cfeb5a65d8a572a61fb40c26e8f3d263ee7
zhiqiang.zhou <zhouzhq7@gmail.com>
2018-07-26 20:48:03 +0100

    add weight before generative loss for generator

--- god_config.py
+++ god_config.py
@@ -10,18 +10,18 @@ config.data_tfrecord_dir = './train/dataset_middle1.tf'
 
 "optimization"
 config.batch_size = 64
-config.lr_init = 1e-4
+config.lr_init = 2e-4
 config.beta1 = 0.5
 config.beta2 = 0.9
 
 "loss"
 config.loss_type = 'sigmoid'
 config.recons_loss_w = 40.0
-config.e_adverse_loss_w = 2.0
+config.e_adverse_loss_w = 8.0
 config.g_gen_loss_w = 4.0
 
 config.n_epoch = 1000
-config.lr_decay = 1
+config.lr_decay = 0.99
 
 "generator type"
 config.generator_type = 'dcgan' # or 'dcgan'
@@ -33,4 +33,4 @@ config.summary_dir = './summary'
 config.save_every = 1
 config.num_of_update_for_e_g = 2
 config.hidden_dim = 128
-config.num_of_resblk = 6
+config.num_of_resblk = 4
