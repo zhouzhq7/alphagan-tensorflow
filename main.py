aede92446168130bd008b3bbeeaa74ce262b053b
zhouzhq7 <zhouzhq7@gmail.com>
2018-08-01 16:09:58 +0100

    add encode and generate part

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from utils import *
 import time
 import numpy as np
 import argparse
-
+from scipy import misc
+import pickle
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
@@ -362,69 +363,80 @@ def train():
         print ("training is done")
         pass
 
-def evaluate(sub='generator', num=16):
+def generate():
     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
-    result_dir = './results'
+    result_dir = './results/recons'
     checkpoints_dir = "./checkpoints"
     mkdir_if_not_exists(result_dir)
-    if sub == 'generator':
-        with tf.name_scope('evaluation'):
-            t_z = tf.placeholder(tf.float32, [num, hidden_dim], name='test_sampled_prior')
-            net_g_test = generator(t_z, is_train=False, reuse=False)
 
-            tl.layers.initialize_global_variables(sess)
+    with open('./results/encoded_feat.pkl', 'rb') as f:
+        feat_dict = pickle.load(f)
 
-            tl.files.load_and_assign_npz(sess=sess,
-                                         name=checkpoints_dir+"/g_{}.npz".format(tl.global_flag['mode']),
-                                         network=net_g_test)
-            sampled_z = np.random.normal(0.0, 1.0, [num, hidden_dim])
-            out = sess.run(net_g_test.outputs,
-                           {t_z: sampled_z})
-            out = (out+1)*127.5
-            print ("gen sub image:", out.shape, out.min(), out.max())
-            print("[*] save images")
-            tl.vis.save_images(out.astype(np.uint8), [4, 4], result_dir+ '/test_'+sub+'.png')
+    encoded_feats = feat_dict['encoded_feats']
+    image_ids = feat_dict['image_ids']
+
+    t_z = tf.placeholder(tf.float32, [None, hidden_dim], name='test_prior')
+    net_g_test, _ = generator(t_z, is_train=False, reuse=False)
+
+    tl.layers.initialize_global_variables(sess)
+
+    tl.files.load_and_assign_npz(sess=sess,
+                                 name=checkpoints_dir+"/g_train.npz",
+                                 network=net_g_test)
+    for i in range(len(image_ids)):
+        cur_feat = np.reshape(encoded_feats[i,:], (-1, 128))
+        out = sess.run(net_g_test.outputs, feed_dict={t_z: cur_feat})
+        out = (out+1)*127.5
+        out = np.reshape(out, (64, 64, 3))
+        misc.imsave(result_dir+'/'+image_ids[i], out.astype(np.uint8))
 
 def encode():
     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
     result_dir = './results'
     checkpoints_dir = "./checkpoints"
     mkdir_if_not_exists(result_dir)
-    with tf.name_scope('evaluation'):
-        t_image = tf.placeholder(tf.float32, [None, 64, 64, 3], name='Input_images')
-        net_e_test, z_test = encoder((t_image/127.5)-1, num_of_resblock=num_of_resblk,
-                                     h_dim=hidden_dim, is_train=False, reuse=True)
-        tl.files.load_and_assign_npz(sess=sess,
-                                     name=checkpoints_dir+"/e_train.npz", network=net_e_test)
+    t_image = tf.placeholder(tf.float32, [None, 64, 64, 3], name='Input_images')
+    net_e_test, z_test = encoder((t_image/127.5)-1, num_of_resblock=num_of_resblk,
+                                 h_dim=hidden_dim, is_train=False, reuse=False)
+    tl.layers.initialize_global_variables(sess)
+    tl.files.load_and_assign_npz(sess=sess,
+                                 name=checkpoints_dir+"/e_train.npz", network=net_e_test)
 
-        encoded_feats = []
-        images_dir = './data'
+    encoded_feats = None
+    images_dir = './train_samples'
 
-        image_paths = glob.glob(images_dir+'/*')
+    image_paths = glob.glob(images_dir+'/*')
 
-        image_ids = []
-        images = []
+    image_ids = []
+    images = []
 
-        for img_path in image_paths:
-            image_id = img_path.split('/')[-1]
-            im = misc.imread(img_path)
-            im = misc.imresize(im, [64, 64])
+    for img_path in image_paths:
+        image_id = img_path.split('/')[-1]
+        im = misc.imread(img_path)
+        im = misc.imresize(im, [64, 64])
 
-            image_ids = np.vstack((image_ids, image_id))
-            images = np.vstack((images, im))
+        image_ids.append(image_id)
 
-        num_of_sample = images.shape[0]
+        images.append(im)
+    images = np.array(images)
+    num_of_sample = images.shape[0]
 
-        num_of_batches = (num_of_sample // batch_size) + 1
+    num_of_batches = (num_of_sample // batch_size) + 1
 
-        for i in range(num_of_batches):
-            start_idx = i*batch_size
-            end_idx = (i+1)*batch_size
-            cur_batch = images[start_idx:end_idx, :,:,:]
-            cur_encoded_feat = sess.run(z_test, feed_dict={t_image: cur_batch})
+    for i in range(num_of_batches):
+        start_idx = i*batch_size
+        end_idx = (i+1)*batch_size
+        cur_batch = images[start_idx:end_idx, :,:,:]
+        cur_encoded_feat = sess.run(z_test, feed_dict={t_image: cur_batch})
+        if encoded_feats is None:
+            encoded_feats = cur_encoded_feat
+        else:
             encoded_feats = np.vstack((encoded_feats, cur_encoded_feat))
 
-        tl.files.save_any_to_npy({'image_ids':image_ids, 'encoded_feats':encoded_feats}, name='./data/encoded_feat.npz')
+
+    with open(result_dir+'/encoded_feat.pkl', 'wb') as f:
+        pickle.dump({'image_ids':image_ids, 'encoded_feats':encoded_feats}, f)
+
 
 
 
@@ -434,12 +446,12 @@ if __name__== "__main__":
 
     tl.global_flag['mode'] = args.mode
 
-    args.mode = 'encode'
+    args.mode = 'gen'
 
     if args.mode == 'train':
         train()
-    elif args.mode == 'evaluate':
-        evaluate()
+    elif args.mode == 'gen':
+        generate()
     elif args.mode == 'encode':
         encode()
     else:
